1e2d5b038257e1b06f5313b9418686778ec198f4
Ambroise Gilbert <ambroise.gilbert8@etu.univ-lorraine.fr>
2025-12-12 09:04:01 +0100

    extends scene pour les vues principales

--- src/main/java/com/example/trello/VueGantt.java
+++ src/main/java/com/example/trello/VueGantt.java
@@ -1,7 +1,13 @@
 package com.example.trello;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
-class VueGantt implements Observateur {
+class VueGantt extends Scene implements Observateur {
     public actualiser(Sujet s) {
         // TODO
+    }
+
+    public VueGantt(Parent parent, double v, double v1) {
+        super(parent, v, v1);
     }
 }
