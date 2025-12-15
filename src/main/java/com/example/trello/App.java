ccc9be2b64d9f0cab917ef27038838aa6826f481
Ambroise Gilbert <ambroise.gilbert8@etu.univ-lorraine.fr>
2025-12-15 10:27:12 +0100

    Ajout App

---
+++ src/main/java/com/example/trello/App.java
@@ -0,0 +1,12 @@
+package com.example.trello;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
