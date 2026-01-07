f7ffd32fcee4a8d928af707255e3bc067079cab3
Ambroise Gilbert <ambroise.gilbert8@etu.univ-lorraine.fr>
2026-01-07 07:49:48 +0100

    ajout de la classe jour

---
+++ src/main/java/com/example/trello/Modele/Jour.java
@@ -0,0 +1,32 @@
+package com.example.trello.Modele;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+public class Jour implements Serializable {
+    private String jour;
+
+    public static final List<String> JOURS_AUTORISES = Arrays.asList(
+            "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"
+    );
+
+    public Jour(String jour) {
+        if (this.JOURS_AUTORISES.contains(jour)) this.jour = jour;
+        else this.jour = this.JOURS_AUTORISES.get(0);
+    }
+
+    public Jour() {
+        this.jour = this.JOURS_AUTORISES.get(0);
+    }
+
+    public String getJour() {
+        return jour;
+    }
+
+    public int compareTo(Jour jour) {
+        if (jour.getJour() == this.getJour()) return 0;
+        else if(JOURS_AUTORISES.indexOf(this.getJour()) > JOURS_AUTORISES.indexOf(jour.getJour())) return 1;
+        else return -1;
+    }
+}
